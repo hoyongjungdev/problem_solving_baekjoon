e534991f7fc4392aac7cae54955e19394dbee083
Hoyong Jung <hoyong.jung.dev@gmail.com>
2021-02-27 21:36:59 +0900

    update p1012 - check visited before function call

--- p1012/Main.java
+++ p1012/Main.java
@@ -8,6 +8,8 @@ public class Main {
   private int[][] map;
   private int n;
   private int m;
+  int[] dy = new int[]{0, 0, 1, -1};
+  int[] dx = new int[]{1, -1, 0, 0};
 
   public static void main(String[] args) {
     var main = new Main();
@@ -15,14 +17,14 @@ public class Main {
 
     var t = sc.nextInt();
 
-    for (int testcase=0; testcase<t; testcase++) {
+    for (int testcase = 0; testcase < t; testcase++) {
       var m = sc.nextInt();
       var n = sc.nextInt();
       var k = sc.nextInt();
 
       var map = new int[n][m];
 
-      for (int i=0; i<k; i++) {
+      for (int i = 0; i < k; i++) {
         int x = sc.nextInt();
         int y = sc.nextInt();
 
@@ -52,20 +54,26 @@ public class Main {
   }
 
   public int dfs(int y, int x) {
-    if (y < 0 || y >= n || x < 0 || x >= m) {
-      return 0;
-    }
-
     if (this.map[y][x] == 0 || this.visited[y][x] == 1) {
       return 0;
     }
 
     this.visited[y][x] = 1;
 
-    dfs(y + 1, x);
-    dfs(y - 1, x);
-    dfs(y, x + 1);
-    dfs(y, x - 1);
+    for (int i = 0; i < 4; i++) {
+      var ny = y + dy[i];
+      var nx = x + dx[i];
+
+      if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+        continue;
+      }
+
+      if (this.map[ny][nx] == 0 || this.visited[ny][nx] == 1) {
+        continue;
+      }
+
+      dfs(ny, nx);
+    }
 
     return 1;
   }
